ebf490bbdb9f8111b9f732c5a07736e5517b48f5
ultramen6 <roman.buka66@gmail.com>
2024-05-23 20:13:54 +0300

    ADD: init prisma service & user service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,13 @@
 import { Module } from '@nestjs/common'
+import { PrismaService } from './prisma/prisma.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { UserService } from './user/user.service';
+import { UserController } from './user/user.controller';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: []
+  imports: [PrismaModule, UserModule],
+  providers: [PrismaService, UserService],
+  controllers: [UserController]
 })
 export class AppModule { }
