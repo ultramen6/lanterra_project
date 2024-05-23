60a7240005be0ce8b8fd6b2028b964cf2c77c800
ultramen6 <roman.buka66@gmail.com>
2024-05-23 20:44:15 +0300

    FIX: prisma is_blocked

---
+++ prisma/migrations/20240523173404_is_blocked_fix/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "users" ALTER COLUMN "is_blocked" SET DEFAULT false;
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { User } from '@prisma/client'
+import { genSaltSync, hashSync } from 'bcrypt'
+import { PrismaService } from 'src/prisma/prisma.service'
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  private readonly logger = new Logger(UserService.name)
+  constructor(
+    private readonly prismaService: PrismaService,
+    private readonly configService: ConfigService
+  ) { }
+
+  async save(user: Partial<User>) {
+    const hashedPassword = user?.password
+      ? this.hashPassword(user.password)
+      : null
+    const savedUser = this.prismaService.user.upsert({
+      where: { email: user.email },
+      update: {
+        password: hashedPassword ?? undefined,
+        provider: user?.provider ?? undefined,
+        roles: user?.roles ?? undefined,
+        isBlocked: user?.isBlocked ?? undefined
+      },
+      create: {
+        email: user.email,
+        password: hashedPassword,
+        roles: ['USER'],
+        provider: user.provider
+      }
+    })
+  }
+
+  private hashPassword(password: string) {
+    return hashSync(password, genSaltSync(10))
+  }
+}
