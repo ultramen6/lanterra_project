491b57e46a97ba1c92f33c591820165704bf2eb9
ultramen6 <roman.buka66@gmail.com>
2024-05-24 13:48:07 +0300

    ADD: jwtModule, PassportModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,14 @@
-import { Module } from '@nestjs/common';
-import { AuthController } from './auth.controller';
-import { AuthService } from './auth.service';
+import { Module } from '@nestjs/common'
+import { AuthController } from './auth.controller'
+import { AuthService } from './auth.service'
+import { UserService } from 'src/user/user.service'
+import { PassportModule } from '@nestjs/passport'
+import { JwtModule } from '@nestjs/jwt'
+import { options } from './config'
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService],
+  imports: [UserService, PassportModule, JwtModule.registerAsync(options())]
 })
-export class AuthModule {}
+export class AuthModule { }
